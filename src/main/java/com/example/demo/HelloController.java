cd24e178ec1ffabcf69654713ff68a080aafe166
Artem Ishchenko <ext-artem.ishchenko@here.com>
2018-06-03 21:38:28 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+  @GetMapping("/home")
+  String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    model.addAttribute("name", "Bitch");
+    return "home";
+  }
+}
